35de446fec0d2df5886ea7b59ce17921090abfa8
lương viết hoàng <hoangckhg190@gmail.com>
2022-12-10 11:31:33 +0700

    done

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public ActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+}
